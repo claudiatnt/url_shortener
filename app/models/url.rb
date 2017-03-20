e7d2e67a14ebbcc272bb9e7e7cc97d1fe65c5bf4
Claudia Tnt <namting88@gmail.com>
2017-03-20 11:40:59 +0800

    1. In models/url.rb, validation for the long url. 2. In models/url.rb, created a shorten method for long url.

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,7 @@
 class Url < ApplicationRecord
+  validates :long_url, format: { with: /https?:\/\/www.\w*.com/, message: "Your link must start with http:// or https://" }
+
+  def shorten
+    self.short_url = SecureRandom.hex(4) # 5 is how many char wanted to be randomly create
+  end
 end
